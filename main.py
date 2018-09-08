acb8a1e215e6ceeea0a9c41494e8b4a53e96991e
Marinos Poiitis <marinospoet@gmail.com>
2018-09-08 18:41:11 +0300

    Update main according to new class structure

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import DbHandler
-import FeatureExtractor
+from FeatureExtractors.HashtagFeatureExtractor import HashtagFeatureExtractor
+from FeatureExtractors.TweetFeatureExtractor import TweetFeatureExtractor
 
 if __name__ == '__main__':
     db_handler = DbHandler.DbHandler()
 
-    feature_extractor = FeatureExtractor.FeatureExtractor(db_handler)
+    feature_extractor = HashtagFeatureExtractor(db_handler)
+    tweet_feature_extractor = TweetFeatureExtractor(db_handler)
 
     hashtag_features = feature_extractor.get_hashtag_features()
-    tweet_features = feature_extractor.get_tweet_features()
+    tweet_features = tweet_feature_extractor.get_tweet_features()
 
     print(hashtag_features)
     print(tweet_features)
