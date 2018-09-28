d4ab37250ab3687b751d3d462bd16afd90bcb2d9
mpoiitis <marinospoet@gmail.com>
2018-09-28 11:48:39 +0300

    Add label extraction

--- Predictors/NaiveBayes.py
+++ Predictors/NaiveBayes.py
@@ -3,10 +3,11 @@ from FeatureSelection.AutoEncoder import AutoEncoder
 
 class NaiveBayes:
 
+    F = 25
     def __init__(self, train, test, reduce_dimensions=False):
         self.train_data = train
         self.test_data = test
-        #labels = self.extractLabels()
+        labels = self.extractLabels()
 
         if reduce_dimensions:
             train_autoencoder = AutoEncoder(self.train_data)
@@ -15,9 +16,26 @@ class NaiveBayes:
             test_autoencoder = AutoEncoder(self.test_data)
             self.test_data = test_autoencoder.reduce_dimensions()
 
-        #self.train_data["label"] = labels
+        self.train_data["label"] = labels
 
+    def extractLabels(self):
+        """
+        assign a label to every hashtag according to 5 popularity buckets
+        """
+        labels = []
+        for _, row in self.train_data.iterrows():
+            if row["popularity"] <= self.F:
+                labels.append(0)
+            elif row["popularity"] > self.F and row["popularity"] <= 2*self.F:
+                labels.append(1)
+            elif row["popularity"] > 2*self.F and row["popularity"] <= 4*self.F:
+                labels.append(2)
+            elif row["popularity"] > 4*self.F and row["popularity"] <= 8*self.F:
+                labels.append(3)
+            else:
+                labels.append(4)
 
+        return labels
 
     def preprocess(self, data):
 
